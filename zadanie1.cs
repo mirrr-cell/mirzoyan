f4da00d9e2f8ff3732ec7249b22a4d354f9fa783
D <ddd>
2026-09-12 15:34:25 +0900

    Первое задание

---
+++ zadanie1.cs
@@ -0,0 +1,26 @@
+/*
+using System;
+
+class Prog
+{
+   static void Main()
+    {
+        Console.Write("Введите длину: ");
+
+        string? input = Console.ReadLine(); 
+        double len = Convert.ToDouble(input);
+
+        Console.Write("Введите ширину: ");
+        input = Console.ReadLine();
+        double wid = Convert.ToDouble(input);
+
+        double area = len * wid;
+        double perimeter = 2 * (len + wid);
+
+        Console.WriteLine($"Площадь: {area}");
+        Console.WriteLine($"Периметр: {perimeter}");
+
+        Console.ReadKey();
+    }
+}
+*/
