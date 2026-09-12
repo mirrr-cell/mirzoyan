66c00c5f2443125384f1c1b5e50e4c1f577e2008
D <ddd>
2026-09-12 15:37:51 +0900

    Третье задание

---
+++ zadanie3.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+class Prog
+   {
+     static void Main()
+    {
+        {
+            Console.WriteLine($"Введите темпеуратру за все 7 дней");
+            double[] t = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            
+        
+
+        Console.WriteLine();
+    }
+   }
+   }
