1aa043c67dd0017ce39724dcdacdbf15fb957e68
D <ddd>
2026-09-12 15:38:44 +0900

    Четвертое задание

---
+++ zadanie4.cs
@@ -0,0 +1,29 @@
+/*
+using System;
+
+class Prog
+   {
+     static void Main()
+    { 
+        double sum1 = 0;
+        double sum2 = 0;
+        double sum3 = 0;
+        Console.WriteLine("Введите 5 оценок ддля студента");
+        for (int i = 0; i < 5; i++)
+        {
+            sum1 += double.Parse(Console.ReadLine());
+            sum2 += double.Parse(Console.ReadLine());
+            sum3 += double.Parse(Console.ReadLine());
+
+            Console.WriteLine(sum1/5);
+            Console.WriteLine(sum2/5);
+            Console.WriteLine(sum3/5);
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
+*/
