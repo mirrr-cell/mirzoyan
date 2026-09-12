02b00403a5afe2f1ddf1217dd389ad9d2a07cff7
D <ddd>
2026-09-12 15:37:01 +0900

    Второе задание

---
+++ zadanie2.cs
@@ -0,0 +1,20 @@
+/*
+using System;
+
+class Prog
+    {
+     static void Main()
+    {
+        Console.Write("ВВЕДИТЕ ТЕКУЩИЙ ДОЛЛАР");
+        double dollar = double.Parse(Console.ReadLine());
+        Console.Write("Ввкедите кол-во рублей");
+        double rub = double.Parse(Console.ReadLine());
+
+
+        double dollars = rub / dollar;
+        Console.WriteLine(Math.Round(dollars, 2));
+        Console.ReadKey();
+        
+    }
+}
+*/
