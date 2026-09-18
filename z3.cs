82252e107260eff8aef3a41451b40c53756b4f35
D <ddd>
2026-09-18 13:07:23 +0900

    третье

---
+++ z3.cs
@@ -0,0 +1,30 @@
+
+/*
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+{
+    List<int> num = new List<int>();
+    HashSet<int> s = new HashSet<int>();
+
+    while (true)
+    {
+    Console.Write("введите число: ");
+    int numb = int.Parse(Console.ReadLine());
+
+    if (s.Contains(numb))
+    {
+    Console.WriteLine($"повторение");
+        break;
+    }
+
+    num.Add(numb);
+    s.Add(numb);
+    }
+
+    Console.WriteLine("введенные числа: " + string.Join(", ", num));
+    }
+}
+*/
